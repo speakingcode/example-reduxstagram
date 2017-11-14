f73019d4d91270369db214de8fa2c249d9ff3019
speakingcode <dlissner@gmail.com>
2017-11-14 01:16:23 -0600

    Display photo and comment section on single view

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
+import Photo from './Photo'
+import Comments from './Comment.js'
+
 const Single = React.createClass({
   render() {
+    let { postId } = this.props.params
+    let index = this.props.posts.findIndex((post) => {
+      return post.code === postId
+    })
+
+    let post = this.props.posts[index]
+
+    let postComments = this.props.comments[postId] ||  []
+
     return(
       <div className="single-photo">
-I am a single photo
+        <Photo
+          { ...this.props }
+          index = { index }
+          post  = { post }
+        />
+        <Comments postComments={postComments}/>
       </div>
     )
   }
